c19954f7f01c502834ff0c1feb40835f6327c73e
Christian Ott <christian.ott@mibexsoftware.com>
2022-03-29 12:44:01 +0200

    Test multple swagger files

--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,8 @@ mkdir('-p', 'web_deploy')
 
 cp('-R', 'web/*', 'web_deploy/');
 
-exec('npm run swagger bundle --        -o web_deploy/swagger.json');
-exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
+exec('npm run swagger bundle --        -o web_deploy/swagger_6.x.json -b spec/6.x/');
+exec('npm run swagger bundle -- --yaml -o web_deploy/swagger_6.x.yaml -b spec/6.x/');
 
-
+exec('npm run swagger bundle --        -o web_deploy/swagger_5.x.json -b spec/5.x/');
+exec('npm run swagger bundle -- --yaml -o web_deploy/swagger_5.x.yaml -b spec/5.x/');
